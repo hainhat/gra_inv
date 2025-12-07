7843ee8b69ffa311dc926b227a5cca28b6e7ea90
hainhat <tohainhat17062003@gmail.com>
2025-12-07 15:52:46 +0700

    Update email.go

--- backend/utils/email.go
+++ backend/utils/email.go
@@ -77,7 +77,7 @@ func SendRSVPConfirmation(toEmail, guestName string) error {
             <div class="content">
                 <h2>Xin chào {{.GuestName}}!</h2>
                 <p>Cảm ơn bạn đã dành thời gian phản hồi lời mời tham dự lễ tốt nghiệp của mình.</p>
-				<p>Nếu có nhu cầu, hãy nhấp vào <a href="https://calendar.app.google/ns4vqiWnxy2PiYpdA">đây</a> để thêm sự kiện này vào ứng dụng Lịch trên điện thoại và nhận thông báo nhé!</p>
+				<p>Nếu có nhu cầu, hãy nhấp vào <a href="https://calendar.app.google/uX6cR4BqkqQRan817">đây</a> để thêm sự kiện này vào ứng dụng Lịch trên điện thoại và nhận thông báo nhé!</p>
                 <p>Chúc bạn thật nhiều sức khoẻ, niềm vui và có một mùa Giáng Sinh an lành!</p>
             </div>
             <div class="footer">
